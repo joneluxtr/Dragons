3d5b0b1bb8da24475b02a3d0603762c2e402b06a
ik <ik@hot.com>
2025-02-25 15:49:10 +0100

    4. Implement adding new Rocket and Mission into DragonsRepository

--- src/main/java/com/ik/dragons/repository/DragonsRepositoryController.java
+++ src/main/java/com/ik/dragons/repository/DragonsRepositoryController.java
@@ -5,24 +5,35 @@ import com.ik.dragons.repository.entity.MissionStatus;
 import com.ik.dragons.repository.entity.Rocket;
 import com.ik.dragons.repository.entity.RocketStatus;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class DragonsRepositoryController implements IDragonsRepository {
+
+    private final Map<Rocket, RocketStatus> rockets = new HashMap();
+    private final Map<Mission, MissionStatus> missions = new HashMap();
+
 
     @Override
     public Rocket addNewRocket(String name) {
-        return null;
+        var rocket = new Rocket(name);
+        rockets.put(rocket, RocketStatus.ON_GROUND);
+        return rocket;
     }
 
     @Override
     public Mission addNewMission(String name) {
-        return null;
+        var mission = new Mission(name);
+        missions.put(mission, MissionStatus.SCHEDULED);
+        return mission;
     }
 
     public RocketStatus getRocketStatus(Rocket rocket) {
-        return null;
+        return rockets.get(rocket);
     }
 
     public MissionStatus getMissionStatus(Mission mission) {
-        return null;
+        return missions.get(mission);
     }
 
 }
